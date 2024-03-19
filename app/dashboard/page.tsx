adb48cec7c2f2dbc171346d250d0567ce78d65a3
汤哲晔 <tangzy@microseer.com>
2024-03-19 18:03:40 +0800

    Update page.tsx
    开始做SSC

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatesInvoice from "@/app/ui/dashboard/latest-invoices";
+import { lusitana  } from "@/app/ui/fonts";
+
 export default function Page () {
-    return <p>Dashboard Page</p>
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap06 md:grid-cols-4 lg:grid-cols-8">
+
+            </div>
+        </main>
+    )
 }
