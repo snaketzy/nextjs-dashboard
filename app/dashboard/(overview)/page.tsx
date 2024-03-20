b1368de7d4bd2a3a7589a7430c2f7b59f238fd7f
汤哲晔 <tangzy@microseer.com>
2024-03-20 13:03:13 +0800

    add LatestInvoicesSkeleton

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
  
 export default async function Page() {
@@ -30,7 +30,9 @@ export default async function Page() {
         <Suspense fallback = { <RevenueChartSkeleton/> }>
             <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback = { <LatestInvoicesSkeleton/> }>
+            <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
